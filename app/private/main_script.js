4cac090921a11ceeb646336091b42f740b50b8a8
AndreaDipa <57904745+AndreaDipa@users.noreply.github.com>
2021-05-25 18:51:02 +0200

    Update main_script.js

--- app/private/main_script.js
+++ app/private/main_script.js
@@ -1,10 +1,4 @@
 $(document).ready(() => {
-    function getCookie(name) {
-         const value = `; ${document.cookie}`;
-         const parts = value.split(`; ${name}=`);
-         if (parts.length === 2) return parts.pop().split(';').shift();
-     }
-     const x = getCookie('x-auth-token');
      let tit, desc;
      $.ajax({
          url: '/api/events',
