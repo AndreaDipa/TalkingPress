14e71cf59ea5a914c5311c598e56f22e73950a45
antonysmart <antonyf99@gmail.com>
2021-05-30 00:05:15 +0200

    Fix news cards

--- app/private/scripts/profile_script.js
+++ app/private/scripts/profile_script.js
@@ -12,6 +12,7 @@ const app = Vue.createApp({
     },
 
     created() {
+        var self = this;
         const sPageURL = window.location.search.substring(1);
         
         $.ajax({
@@ -20,36 +21,64 @@ const app = Vue.createApp({
             dataType: "json",
             success: function(res) {
                 var myEvents = res.events;
-                var ul = document.getElementById("stories-ul");
-                
+                self.stories = myEvents;
+
+                var event;
+
                 for (i = 0; i < myEvents.length; i++) {
-                    var event = myEvents[i];
-                    var li = document.createElement('li');
-
-                    li.setAttribute('class', "list-group-item col-12 col-md-6");
-
-                    var div1 = li.appendChild(document.createElement('div'));
-                    var h3 = div1.appendChild(document.createElement('h3'));
-                    var h6 = div1.appendChild(document.createElement('h6'));
-
-                    h3.appendChild(document.createTextNode(event.title));
-                    h6.appendChild(document.createTextNode(event.description));
-
-                    var hr = div1.appendChild(document.createElement('hr'));
-
-                    var div2 = li.appendChild(document.createElement('div'));
-                    var h5 = div2.appendChild(document.createElement('h5'));
-
+                    event = myEvents[i];
+                    
+                    self.title = event.title;
+                    self.description = event.description;
                     if(event.comment == "") {
-                        var italic = h5.appendChild(document.createElement('i'));
-                        var nocomment = italic.appendChild(document.createTextNode("NO COMMENT"));
-                        h5.setAttribute('style', "color: gray");
+                        self.comment = "[NO COMMENT]";
+                    } else {
+                        self.comment = event.comment;
                     }
+                }
+            },
+                /*var ul = document.getElementById("stories-ul");
+                
+                var event = myEvents[i];
+                
+                if(myEvents.length == 0) {
+                    var h1 = document.createElement('h1');
+                    var italich1 = h1.appendChild(document.createElement('i'));
+                    var nostories = italich1.appendChild(document.createTextNode("Nothing to show"));
+                    h5.setAttribute('style', "color: gray");
+                } else {
+                    for (i = 0; i < myEvents.length; i++) {
+                        var li = document.createElement('li');
 
-                    h5.appendChild(document.createTextNode(event.comment));
+                        li.setAttribute('class', "list-group-item col-12 col-md-6");
 
-                    ul.appendChild(li);
-                }               
+                        var div1 = li.appendChild(document.createElement('div'));
+                        var h3 = div1.appendChild(document.createElement('h3'));
+                        var h6 = div1.appendChild(document.createElement('h6'));
+
+                        h3.appendChild(document.createTextNode(event.title));
+                        h6.appendChild(document.createTextNode(event.description));
+
+                        var hr = div1.appendChild(document.createElement('hr'));
+
+                        var div2 = li.appendChild(document.createElement('div'));
+                        var h5 = div2.appendChild(document.createElement('h5'));
+
+                        if(event.comment == "") {
+                            var italich5 = h5.appendChild(document.createElement('i'));
+                            var nocomment = italich5.appendChild(document.createTextNode("NO COMMENT"));
+                            h5.setAttribute('style', "color: gray");
+                        }
+
+                        h5.appendChild(document.createTextNode(event.comment));
+
+                        ul.appendChild(li);
+                    }   
+                }*/          
+            
+
+            error: function(err) {
+                console.log("error ajax with profile data");
             },
         });
     },
