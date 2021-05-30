62b8e44d8c6727e53fb99dbca931985a912630ac
antonysmart <antonyf99@gmail.com>
2021-05-30 09:27:22 +0200

    Added two buttons edit & delete

--- app/private/scripts/profile_script.js
+++ app/private/scripts/profile_script.js
@@ -23,6 +23,12 @@ const app = Vue.createApp({
                 var myEvents = res.events;
                 self.stories = myEvents;
 
+                if(myEvents.length == 0) {
+                    var nothing = document.createElement('h1');
+                    nothing.appendChild(document.createTextNode("Nothing to show"));
+                    nothing.setAttribute('style', "color: gray");
+                }
+
                 var event;
 
                 for (i = 0; i < myEvents.length; i++) {
@@ -37,46 +43,7 @@ const app = Vue.createApp({
                     }
                 }
             },
-                /*var ul = document.getElementById("stories-ul");
-                
-                var event = myEvents[i];
-                
-                if(myEvents.length == 0) {
-                    var h1 = document.createElement('h1');
-                    var italich1 = h1.appendChild(document.createElement('i'));
-                    var nostories = italich1.appendChild(document.createTextNode("Nothing to show"));
-                    h5.setAttribute('style', "color: gray");
-                } else {
-                    for (i = 0; i < myEvents.length; i++) {
-                        var li = document.createElement('li');
-
-                        li.setAttribute('class', "list-group-item col-12 col-md-6");
-
-                        var div1 = li.appendChild(document.createElement('div'));
-                        var h3 = div1.appendChild(document.createElement('h3'));
-                        var h6 = div1.appendChild(document.createElement('h6'));
-
-                        h3.appendChild(document.createTextNode(event.title));
-                        h6.appendChild(document.createTextNode(event.description));
-
-                        var hr = div1.appendChild(document.createElement('hr'));
-
-                        var div2 = li.appendChild(document.createElement('div'));
-                        var h5 = div2.appendChild(document.createElement('h5'));
-
-                        if(event.comment == "") {
-                            var italich5 = h5.appendChild(document.createElement('i'));
-                            var nocomment = italich5.appendChild(document.createTextNode("NO COMMENT"));
-                            h5.setAttribute('style', "color: gray");
-                        }
-
-                        h5.appendChild(document.createTextNode(event.comment));
-
-                        ul.appendChild(li);
-                    }   
-                }*/          
             
-
             error: function(err) {
                 console.log("error ajax with profile data");
             },
