684467ebad3d69f3a58110690bb074d9e14f88ff
antonysmart <antonyf99@gmail.com>
2021-05-30 18:25:39 +0200

    fix delete button: edit button not working

--- app/private/scripts/profile_script.js
+++ app/private/scripts/profile_script.js
@@ -15,12 +15,17 @@ const app = Vue.createApp({
     created() {
         var self = this;
         const sPageURL = window.location.search.substring(1);
-        
+
         $.ajax({
             url: '/api/users/me',
             type: "GET",
             dataType: "json",
             success: function(res) {
+                //generate username
+                $('#hellouser').html(res.username);
+
+
+                //generate news cards
                 var myEvents = res.events;
                 self.stories = myEvents;
 
@@ -46,41 +51,6 @@ const app = Vue.createApp({
                     }*/
                     self.comment = event.comment;
 
-                    $("#deletebutton").click(() => {
-                        $.ajax({
-                            url: "/api/events/" + self._id,
-                            type: "DELETE",
-                            dataType: "json",
-                            success: function (data) {
-                                console.log("deleted");
-                                window.location.reload();
-                            },
-                            error: function (err) {
-                                console.log("error ajax");
-                            },
-                        });
-                    });
-
-                    $("#salva").click(() => {
-                        $.ajax({
-                            url: "/api/events/" + self._id,
-                            type: "PUT",
-                            dataType: "json",
-                            contentType: "application/json",
-                            data: JSON.stringify({
-                                comment: $("#comment").val(),
-                            }),
-                            success: function (data) {
-                                self.comment = data.comment;
-                                console.log("sended");
-                                $("#comment").val("");
-                                window.location.reload();
-                            },
-                            error: function (err) {
-                                console.log("error ajax");
-                            },
-                        });
-                    });
                 }
             },
             
@@ -89,22 +59,93 @@ const app = Vue.createApp({
             },
         });
     },
+    methods: {
+
+        delete_news(id) {
+            var self = this.stories;
+
+            $.ajax({
+                url: "/api/events/" + id,
+                type: "DELETE",
+                success: function (res) {
+                    for (let i = 0; i < Object.keys(self).length; i++) {
+                        if (self[i]._id == id) {
+                            self.splice(i, 1);
+                            break;
+                        }
+                    }
+                    console.log("deleted");
+                },
+                error: function (err) {
+                    console.log("error ajax");
+                },
+            });
+        },
+
+        edit_news(id) {
+            var self = this.stories;
+
+            console.log(id);
+
+            $.ajax({
+                url: "/api/events/" + id,
+                type: "PUT",
+                dataType: "json",
+                data: {
+                    comment: $(`#${id}`).val(),
+                },
+
+                success: function(res) {
+                    //self.comment = data.comment;
+                    console.log(res.comment);
+                    for(let i = 0; i < Object.keys(self).length; i++) {
+                        console.log(self[i]);
+                        if(self[i]._id == id) {
+                            self[i].comment = res.comment;
+                            break;
+                        }
+                        
+                    }
+                    
+                    console.log("sent");
+                    $(`#${id}`).val("");
+                },
+                error: function (err) {
+                    console.log("error ajax");
+                },
+            });
+        }
+    },
+    
 });
 
 app.mount("#stories");
 
-$(document).ready(() => {
-    const sPageURL = window.location.search.substring(1);
+/*const app2 = Vue.createApp({
+    data() {
+        return {
+            _id: "User ID",
+        }
+    },
+    created() {
+        var self = this;
 
-    $.ajax({
-        url: '/api/users/me',
-        type: 'GET',
-        dataType: 'json',
-        success: function(res) {
-            $('#hellouser').html(res.username);
-        },
-        error: function (err) {
-            console.log('error ajax username');
-        },
-    });
-})
+        $("#deletebutton").click(() => {
+            $.ajax({
+                url: "/api/users/" + self._id,
+                type: "DELETE",
+                dataType: "json",
+                success: function () {
+                    console.log("deleted account");
+                    window.location.href('./deleted.html');
+                },
+                error: function (err) {
+                    console.log("error ajax");
+                },
+            });
+        });
+    },
+});
+
+app2.mount("#profile-settings");*/
+
